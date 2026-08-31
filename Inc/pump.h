e53a7c9e9f479fa2600c072e9609877754966936
Mahi9033 <patilmahi9033@gmail.com>
2026-08-31 23:17:37 +0530

    Add pump control driver

---
+++ Inc/pump.h
@@ -0,0 +1,8 @@
+
+
+#ifndef PUMP_H_
+#define PUMP_H_
+
+void pump_off(void);
+void pump_on(void);
+#endif /* PUMP_H_ */
---
+++ Src/pump.c
@@ -0,0 +1,29 @@
+#include "stm32_regs.h"
+#include <stdint.h>
+
+#define GPIOAEN        (1 << 0)
+#define PUMP_ON        (1 << 6)
+void pump_on(void){
+	RCC->AHB1ENR |= GPIOAEN;
+
+	GPIOA->MODER |= (1 << 12);
+	GPIOA->MODER &= ~(1 << 13);
+
+	GPIOA->ODR |= PUMP_ON;
+
+
+
+}
+
+void pump_off(void){
+	RCC->AHB1ENR |= GPIOAEN;
+
+	GPIOA->MODER |= (1 << 12);
+	GPIOA->MODER &= ~(1 << 13);
+
+	GPIOA->ODR &= ~PUMP_ON;
+
+
+
+}
+

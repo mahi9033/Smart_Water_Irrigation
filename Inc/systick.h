4811af0677f9d8373cac2d8790fef881be04b755
Mahi9033 <patilmahi9033@gmail.com>
2026-09-04 15:52:04 +0530

    Update SysTick driver header

--- Inc/systick.h
+++ Inc/systick.h
@@ -8,14 +8,11 @@
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
 
-#ifndef SYSTICK_H_
-#define SYSTICK_H_
 
 #include <stdint.h>
-
 void systick_init(void);
 void systick_delay_ms(uint32_t delay);
 
-#endif
+
 
 #endif /* SYSTICK_H_ */

039e9fb8912aba5daa1c0d135267c372ce25b0a7
Mahi9033 <patilmahi9033@gmail.com>
2026-09-04 15:52:12 +0530

    Add water level header

---
+++ Inc/water_level.h
@@ -0,0 +1,13 @@
+/*
+ * water_level.h
+ *
+ *  Created on: Sep 4, 2026
+ *      Author: Mahi
+ */
+
+#ifndef WATER_LEVEL_H_
+#define WATER_LEVEL_H_
+#include <stdint.h>
+uint8_t water_level(void);
+
+#endif /* WATER_LEVEL_H_ */

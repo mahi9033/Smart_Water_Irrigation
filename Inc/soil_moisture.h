769bd5f729a5d2a768d70e53f7ba1573c32cb627
Mahi9033 <patilmahi9033@gmail.com>
2026-09-04 15:51:11 +0530

    Add soil moisture header

---
+++ Inc/soil_moisture.h
@@ -0,0 +1,15 @@
+/*
+ * soil_moisture.h
+ *
+ *  Created on: Sep 4, 2026
+ *      Author: Mahi
+ */
+
+#ifndef SOIL_MOISTURE_H_
+#define SOIL_MOISTURE_H_
+#include <stdint.h>
+uint8_t soil_moisture(void);
+uint8_t soil_sensor_health(void);
+
+
+#endif /* SOIL_MOISTURE_H_ */

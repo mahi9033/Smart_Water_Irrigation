4f6976e295ae133712e7bf987c852a6fe0ae306e
Mahi9033 <patilmahi9033@gmail.com>
2026-09-04 15:55:28 +0530

    Update main system integration

--- Src/main.c
+++ Src/main.c
@@ -1,16 +1,28 @@
 #include "stm32_regs.h"
 #include "gpio.h"
 #include "adc.h"
-
+#include "i2c.h"
+#include "lcd.h"
+#include "pump.h"
+#include "water_level.h"
+#include "soil_moisture.h"
+#include "emergency_stop.h"
+#include "mannual.h"
+#include "irigation.h"
+#include "systick.h"
 uint32_t soil_val;
 
 int main(void)
 {
 	gpio_init();
 	adc_init();
-	start_conv();
+	i2c_init();
+	systick_init();
+	lcd_init();
+	emergency_stop_init();
+
     while (1)
     {
-    	soil_val = read_data();
+    	irrigation_control();
     }
 }

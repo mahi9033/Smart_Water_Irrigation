41b916f95503e7c8c191ea328a886ef8e2d73c63
Mahi9033 <patilmahi9033@gmail.com>
2026-09-04 15:50:28 +0530

    Add irrigation control header

---
+++ Inc/irigation.h
@@ -0,0 +1,14 @@
+/*
+ * irigation.h
+ *
+ *  Created on: Sep 4, 2026
+ *      Author: Mahi
+ */
+
+#ifndef IRIGATION_H_
+#define IRIGATION_H_
+
+void irrigation_control(void);
+
+
+#endif /* IRIGATION_H_ */
